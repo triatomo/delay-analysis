3698669357c093f48d111738d9c3b7572f75aae5
Restria Hertomo <restria.hertomo@gmail.com>
2019-07-10 20:10:15 +0200

    Adding delay vs. countries and carriers

--- eda.py
+++ eda.py
@@ -81,6 +81,29 @@ plt.xlabel(' ')
 plt.savefig('FDA-Delivery vs. delay.png')
 plt.show()
 
+""" Number of shipments vs. countries vs. delay """
+g=sns.countplot(x='Customer Address Country',hue='Delayed', data=df, order=["AT", "FR", "DE", "ES", "IT", "CH", "LU"])
+
+# put values on the bars
+for i in g.patches:
+    height = i.get_height()
+    g.text(i.get_x() + i.get_width()/2, 200+height, str(i.get_height()), color="black", ha="center")
+
+plt.savefig('Delayed shipments vs. countries.png')
+plt.show()
+
+""" Delay probability vs. countries """
+g=sns.barplot(x='Customer Address Country', y='Delayed', data=df, ci=None, order=["AT", "FR", "DE", "ES", "IT", "CH", "LU"])
+
+# put values on the bars
+for i in g.patches:
+    height = i.get_height()
+    g.text(i.get_x() + i.get_width()/2, 0.001+height, round(i.get_height(),3), color="black", ha="center")
+
+plt.ylabel('Delay rate')
+plt.savefig('Delay rate by countries.png')
+plt.show()
+
 """ Carrier Company vs. Delay """
 g=sns.barplot(x='Carrier', y='Delayed', data=df, ci=None)
 
