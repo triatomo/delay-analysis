af5585a81b5c11b381382a796aae767d244ab09f
Restria Hertomo <restria.hertomo@gmail.com>
2019-07-10 16:08:41 +0200

    Adding FHS-FDA

--- eda.py
+++ eda.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('cleaned_data.csv', dtype={'Delayed': np.bool})
-# pd.isnull(df['Delayed']).values.any()
 df['all'] = "" 
 
+""" PPU-FHS vs. Delay """
 q99 = df["PPU-FHS"].quantile(0.99)
 
 plt.subplot(2,1,1)
@@ -21,8 +21,26 @@ plt.xlabel(' ')
 
 plt.subplot(2,1,2)
 g = sns.violinplot(y='PPU-FHS', x='all', hue='Delayed', split=True, data=df[df['PPU-FHS']<q99])
-
 plt.ylabel('Days')
 plt.xlabel(' ')
-plt.savefig('PPU-FHS vs. delay.png')
+
+# plt.savefig('PPU-FHS vs. delay.png')
+plt.show()
+
+""" FHS-FDA vs. Delay """
+q99 = df["FHS-FDA"].quantile(0.99)
+
+plt.subplot(2,1,1)
+plt.title("FHS-FDA")
+g = sns.violinplot(y='FHS-FDA', x='all', hue='Delayed', split=True, data=df[df['FHS-FDA']>=q99])
+plt.yscale('log', basey=2)
+plt.ylabel('Days')
+plt.xlabel(' ')
+
+plt.subplot(2,1,2)
+g = sns.violinplot(y='FHS-FDA', x='all', hue='Delayed', split=True, data=df[df['FHS-FDA']<q99])
+plt.ylabel('Days')
+plt.xlabel(' ')
+
+plt.savefig('FHS-FDA vs. delay.png')
 plt.show()
